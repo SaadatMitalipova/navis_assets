c7467ebc4954633b03f389e224e4e5b59bdecd09
saadat <saadat0989@gmail.com>
2024-08-22 16:50:44 +0600

    [200~Wallet Recovery Promo #3

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,11 +1,14 @@
 // import ".";
 
 import Header from "../../components/header/header";
+import HeroSection from "../../components/heroSection/heroSection";
 
 function Home() {
   return (
     <>
       <Header />
+      <hr />
+      <HeroSection/>
     </>
   );
 }
